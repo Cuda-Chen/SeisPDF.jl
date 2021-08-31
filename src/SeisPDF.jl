969ad2620d3ca9c1ff65d3f38d3868afb6837554
Cuda Chen <clh960524@gmail.com>
2021-08-31 22:11:53 +0800

    add main SeisPDF file

---
+++ src/SeisPDF.jl
@@ -0,0 +1,19 @@
+# Read input trace
+
+# Read response file
+
+# Split trace to 1-hour then 15-minute
+
+# For each 15-minute segment
+# Detrend
+
+# Demean
+
+# FFT
+
+# Remove response
+
+# Calculate PSD
+# end segment
+
+# Calculate PDF summary
