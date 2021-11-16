14aa2b2efe380cdd30156d6d76f46c81effc69d1
Cuda Chen <clh960524@gmail.com>
2021-11-16 22:52:14 +0800

    restore accidently commented tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -4,12 +4,12 @@ using SeisIO: d2u
 
 using SeisPDF
 
-#include("test_slicing.jl")
-#include("test_read_response.jl")
-#include("test_demean.jl")
-#include("test_detrend.jl")
-#include("test_taper.jl")
-#include("test_compute_fft.jl")
-#include("test_calculate_psd.jl")
-#include("test_remove_response.jl")
+include("test_slicing.jl")
+include("test_read_response.jl")
+include("test_demean.jl")
+include("test_detrend.jl")
+include("test_taper.jl")
+include("test_compute_fft.jl")
+include("test_calculate_psd.jl")
+include("test_remove_response.jl")
 include("test_summarize_psd.jl")
