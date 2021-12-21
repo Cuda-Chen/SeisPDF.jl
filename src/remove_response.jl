39aaf1f5b0774bc1a333d53bbe00d8cae429650d
Cuda Chen <clh960524@gmail.com>
2021-12-21 23:21:26 +0800

    fix OOM when remove_response with long sample trace

--- src/remove_response.jl
+++ src/remove_response.jl
@@ -1,6 +1,8 @@
 export remove_response!
 
 function remove_response!(data::AbstractArray, freq_response::AbstractArray)
-    data /= freq_response
+    for i in 1:length(data)
+        data[i] /= freq_response[i]
+    end
     data[1] = 0.0 + 0.0 * im # set DC frequency to zero
 end
--- test/test_remove_response.jl
+++ test/test_remove_response.jl
@@ -10,3 +10,13 @@ expect_output_type = Vector{Complex{Float64}}
     @test size(data, 1) == N
     @test typeof(data) == expect_output_type
 end
+
+# remove_response with large samples
+@testset "remove response with large samples" begin
+    samples = 90000
+    data = ones(T, samples)
+    freq_response = ones(T, samples)
+    remove_response!(data, freq_response)
+    @test size(data, 1) == samples
+    @test eltype(data) == T
+end
