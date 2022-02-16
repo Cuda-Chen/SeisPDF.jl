9751d39106b2cef6d13a2d436d23a0a20e56d327
Cuda-Chen <clh960524@gmail.com>
2022-02-16 16:00:37 +0800

    Remove plot_pdf unit test
    Remove plot_pdf unit test as plotting should be tested by GMT.jl.

--- test/test_plot_pdf.jl
+++
@@ -1,8 +0,0 @@
-@testset "plot fake PDF" begin
-    N = 151
-    periods = 102
-    T = Float64
-    fake_pdf = rand(T, (periods, N))
-    @test !isa(try plot_pdf(fake_pdf, show=false) catch ex ex end,
-               Exception)
-end
