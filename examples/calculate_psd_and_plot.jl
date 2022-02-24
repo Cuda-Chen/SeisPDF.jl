40d9552a8d7f2d92e52f52df0ee636a123ec42ea
Cuda Chen <clh960524@gmail.com>
2022-02-24 09:14:26 +0800

    Test grdview parameters

--- examples/calculate_psd_and_plot.jl
+++ examples/calculate_psd_and_plot.jl
@@ -124,23 +124,27 @@ end
 #imshow(pdf_mean_1_hour, x=periods, y=powers; proj=:logx)
 #imshow(pdf_mean_1_hour)
 #imshow(pdf_mean_1_hour, savefig="foo.png", show=false)
-plot_pdf(pdf_mean_1_hour)
+#plot_pdf(pdf_mean_1_hour)
 
 # Plot PDF of this 1-hour slide
-#period_max = log10(maximum(center_periods))
-#period_min = log10(minimum(center_periods))
-#center_periods_interval_in_logscale = log10(center_periods[2]) - log10(center_periods[1])
+period_max = log10(maximum(center_periods))
+period_min = log10(minimum(center_periods))
+center_periods_interval_in_logscale = log10(center_periods[2]) - log10(center_periods[1])
+periods = collect(period_min:period_max:center_periods_interval_in_logscale)
+powers = collect(-200:-50:1)
+pdf_mean_1_hour_min = minimum(pdf_mean_1_hour)
+pdf_mean_1_hour_max = maximum(pdf_mean_1_hour)
+g_cpt = makecpt(color=:rainbow, T="$pdf_mean_1_hour_min/$pdf_mean_1_hour_max")
+grdview(pdf_mean_1_hour, J="X6i/5i", 
+        frame=(xlabel="log10(Period)", ylabel="Power [10log10(m**2/sec**4/Hz)] [dB]", axes=:WSne), 
+        color=g_cpt, S=100,  Q="s", Y="4.0", show=true)
+#grdview(pdf_mean_1_hour, J="X6i/5i", frame=(xlabel="log10(Period)", ylabel="Power [10log10(m**2/sec**4/Hz)] [dB]", axes=:WSne), color=g_cpt, S=100, Q="s", N=0, V=true, Y="4.0", show=true)
 #
 ## Create netCDF grid
 #pdf_mean_1_hour = reshape(pdf_mean_1_hour, :)
 #pdf_mean_1_hour_grid = xyz2grd(pdf_mean_1_hour, R="$period_min/$period_max/-200/-50", I="$center_periods_interval_in_logscale/1", Z="TLA", V=true)
 #
 #
-#pdf_mean_1_hour_min = minimum(pdf_mean_1_hour)
-#pdf_mean_1_hour_max = maximum(pdf_mean_1_hour)
-#g_cpt = makecpt(color=:rainbow, T="$pdf_mean_1_hour_min/$pdf_mean_1_hour_max")
-#periods = collect(pdf_mean_1_hour_min:pdf_mean_1_hour_max:center_periods_interval_in_logscale)
-#powers = collect(-200:-50:1)
 #
 ##grdview(pdf_mean_1_hour_grid, J="X6i/5i", frame=(xlabel="log10(Period)", ylabel="Power [10log10(m**2/sec**4/Hz)] [dB]", axes=:WSne), color=g_cpt, S=100, Q="s", N=0, V=true, Y="4.0", show=true)
 ##grdview(pdf_mean_1_hour_grid, J="X6i/5i", frame=(xlabel="log10(Period)", ylabel="Power [10log10(m**2/sec**4/Hz)] [dB]", axes=:WSne, ), color=g_cpt , V=true, show=true, colorbar=true)
