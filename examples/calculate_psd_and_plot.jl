34640627fc1f82f29c386a1b36e5fb4beb1e42ca
Cuda Chen <clh960524@gmail.com>
2022-02-27 16:37:26 +0800

    All plot related parameter now can be set in one function
    1. All plot related parameter now can be set in one function.
    2. Add doc-string of plot_pdf.

--- examples/calculate_psd_and_plot.jl
+++ examples/calculate_psd_and_plot.jl
@@ -11,31 +11,31 @@ S = read_data("mseed", input_trace_file, memmap=true)
 response = read_resp_from_sacpz(response_file, S.fs[1], length(S.x[1]))
 pdf_mean_1_hour, center_periods = process_one_channel(S, response)
 
-#plot_pdf(pdf_mean_1_hour)
+plot_pdf(pdf_mean_1_hour, center_periods; show=true)
 
 # Plot PDF of this 1-hour slide
-reverse!(pdf_mean_1_hour, dims=1)
-period_max = log10(maximum(center_periods))
-period_min = log10(minimum(center_periods))
-center_periods_interval_in_logscale = log10(center_periods[2]) - log10(center_periods[1])
-periods = collect(period_min:period_max:center_periods_interval_in_logscale)
-powers = collect(-200:-50:1)
-pdf_mean_1_hour_min = minimum(pdf_mean_1_hour)
-pdf_mean_1_hour_max = maximum(pdf_mean_1_hour)
-# Create netCDF grid
-pdf_mean_1_hour = reshape(pdf_mean_1_hour, :)
-pdf_mean_1_hour_grid = xyz2grd(pdf_mean_1_hour, R="$period_min/$period_max/-200/-50", I="$center_periods_interval_in_logscale/1", Z="LBA", V=true)
-#pdf_mean_1_hour_grid = mat2grid(pdf_mean_1_hour, x=center_periods, y=powers)
-g_cpt = makecpt(color=:rainbow, T="$pdf_mean_1_hour_min/$pdf_mean_1_hour_max")
-#grdview(pdf_mean_1_hour_grid, J="X6i/5i", surftype=:surface, plane=(0, :white),
-#        frame=(xlabel="log10(Period)", ylabel="Power [10log10(m**2/sec**4/Hz)] [dB]", axes=:WSne), 
-#        color=g_cpt, S=100, Y="4.0", show=false)
-grdimage(pdf_mean_1_hour_grid, J="X6i/5i", 
-         xaxis=(annot=:auto, ticks=:auto, label="log10(Period)"),
-         yaxis=(annot=:auto, ticks=:auto, label="Power [10log10(m**2/sec**4/Hz)] [dB]"),
-         color=:rainbow, show=false)
-colorbar!(g_cpt, B=0.02, pos=(anchor=:RM, offset=(1.5,0), neon=true),
-          V=true, show=true)
+#reverse!(pdf_mean_1_hour, dims=1)
+#period_max = log10(maximum(center_periods))
+#period_min = log10(minimum(center_periods))
+#center_periods_interval_in_logscale = log10(center_periods[2]) - log10(center_periods[1])
+#periods = collect(period_min:period_max:center_periods_interval_in_logscale)
+#powers = collect(-200:-50:1)
+#pdf_mean_1_hour_min = minimum(pdf_mean_1_hour)
+#pdf_mean_1_hour_max = maximum(pdf_mean_1_hour)
+## Create netCDF grid
+#pdf_mean_1_hour = reshape(pdf_mean_1_hour, :)
+#pdf_mean_1_hour_grid = xyz2grd(pdf_mean_1_hour, R="$period_min/$period_max/-200/-50", I="$center_periods_interval_in_logscale/1", Z="LBA", V=true)
+##pdf_mean_1_hour_grid = mat2grid(pdf_mean_1_hour, x=center_periods, y=powers)
+#g_cpt = makecpt(color=:rainbow, T="$pdf_mean_1_hour_min/$pdf_mean_1_hour_max")
+##grdview(pdf_mean_1_hour_grid, J="X6i/5i", surftype=:surface, plane=(0, :white),
+##        frame=(xlabel="log10(Period)", ylabel="Power [10log10(m**2/sec**4/Hz)] [dB]", axes=:WSne), 
+##        color=g_cpt, S=100, Y="4.0", show=false)
+#grdimage(pdf_mean_1_hour_grid, J="X6i/5i", 
+#         xaxis=(annot=:auto, ticks=:auto, label="log10(Period)"),
+#         yaxis=(annot=:auto, ticks=:auto, label="Power [10log10(m**2/sec**4/Hz)] [dB]"),
+#         color=:rainbow, show=false)
+#colorbar!(g_cpt, B=0.02, pos=(anchor=:RM, offset=(1.5,0), neon=true),
+#          V=true, show=true)
 #grdview(pdf_mean_1_hour, J="X6i/5i", frame=(xlabel="log10(Period)", ylabel="Power [10log10(m**2/sec**4/Hz)] [dB]", axes=:WSne), color=g_cpt, S=100, Q="s", N=0, V=true, Y="4.0", show=true)
 #
 ## Create netCDF grid
--- src/plot_pdf.jl
+++ src/plot_pdf.jl
@@ -1,7 +1,51 @@
 export plot_pdf
 
-using GMT: imshow
+using GMT: xyz2grd, makecpt, grdimage, colorbar!
 
-function plot_pdf(pdf::Array{<:Real, 2}; outputfile="pdf_plot.png", show=true)
-    imshow(reverse(pdf, dims=1), savefig=outputfile, show=show)
+"""
+    plot_pdf(pdf::Array{<:Real, 2}, center_periods::Array{<:Real, 1}; mindb::Real=-200, maxdb::Real=-50, db_interval::Real=1, kw...)
+Plot PDF.
+
+# Arguments
+- `pdf::Array{<:Real, 2}`: the matrix storing PDF.
+- `center_periods::Array{<:Real, 1}`: the center periods.
+
+# Optional
+- `mindb::Real`: the minimum range of PDF. Measured in dB.
+- `maxdb::Real`: the maximum range of PDF. Measured in dB.
+- `db_interval::Real`: the interval of PDF. Measured in dB.
+- `kw`: the keyword arguments passed to GMT for controlling GMT. For example:
+    - You can use `plot_pdf(pdf, center_periods; show=true)` just to show the graph.
+    - You can use `plot_pdf(pdf, center_periods; show=true, savefig="foo.png")` to show and save the graph to "foo.png".
+    - You can use `plot_pdf(pdf, center_periods; show=false, savefig="foo.png")` to just save the graph to "foo.png".
+"""
+function plot_pdf(pdf::Array{<:Real, 2}, center_periods::Array{<:Real, 1}; mindb::Real=-200, maxdb::Real=-50, db_interval::Real=1, kw...)
+    # Reverse the PDF matrix or the result will be up-side-down
+    reverse!(pdf, dims=1)
+
+    period_max = log10(maximum(center_periods))
+    period_min = log10(minimum(center_periods))
+    center_periods_interval_in_logscale = log10(center_periods[2]) - log10(center_periods[1])
+    pdf_min = minimum(pdf)
+    pdf_max = maximum(pdf)
+
+    # Create netCDF grid
+    pdf = reshape(pdf, :)
+    pdf_grid = xyz2grd(pdf,
+                       R="$period_min/$period_max/$mindb/$maxdb",
+                       I="$center_periods_interval_in_logscale/$db_interval",
+                       Z="LBA")
+
+    # Plot
+    g_cpt = makecpt(color=:rainbow, T="$pdf_min/$pdf_max")
+    grdimage(pdf_grid, 
+             J="X6i/5i",
+             xaxis=(annot=:auto, ticks=:auto, label="log10(Period)"),
+             yaxis=(annot=:auto, ticks=:auto, label="Power [10log10(m**2/sec**4/Hz)] [dB]"),
+             color=:rainbow, 
+             show=false)
+    colorbar!(g_cpt,
+              B=0.02, 
+              pos=(anchor=:RM, offset=(1.5,0), neon=true);
+              kw...)
 end
