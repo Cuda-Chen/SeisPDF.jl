4d8c7b4d9fd3f949bf74f46a624595e312cf54a2
Cuda-Chen <clh960524@gmail.com>
2022-03-17 17:24:04 +0800

    Find root cause of wrong PSD calculation of this package
    Find root cause of wrong PSD calculation.
    The root cause is that the PSD should first be averaged per one
    hour then do dimension reduction, whereas I do this together (I
    just sum then average).
    
    Whlist the C version (ms2psd) seems not have this problem, maybe
    some kind of misunderstaning or lack of code-review?

--- examples/calculate_psd_and_plot.jl
+++ examples/calculate_psd_and_plot.jl
@@ -9,10 +9,10 @@ response = read_resp_from_sacpz(response_file, S.fs[1], length(S.x[1]))
 pdf_mean_1_hour, center_periods = process_one_channel(S, response; divide_by_period=true)
 
 # plot using GMT
-plot_pdf(pdf_mean_1_hour, center_periods; show=true)
+#plot_pdf(pdf_mean_1_hour, center_periods; show=true)
 
 # plot using GMT and output the plot to `foo.png`
 #plot_pdf(pdf_mean_1_hour, center_periods; show=false, savefig="foo.png")
 
 # plot using UnicodePlots (for test purpose)
-#plot_pdf_in_unicode(pdf_mean_1_hour)
+plot_pdf_in_unicode(pdf_mean_1_hour)
--- src/summarize_psd.jl
+++ src/summarize_psd.jl
@@ -70,13 +70,17 @@ function summarize_psd(psd_bin::AbstractArray{<:Real, 2}, sampling_rate::Float64
 
         psd_result[4, i] = median(psd_bin[:, i]) # median
     end"""
+    psd_bin_temp = zeros(eltype(psd_bin), N)
+    for i in 1:N
+        psd_bin_temp[i] = mean(psd_bin[:, i])
+    end
 
     # Dimension reduction technique escribed in McNamara 2004
     left_freqs, right_freqs, center_periods = get_freqs_and_periods(sampling_rate, (Int64)(N / sampling_rate), smoothing_width_factor)
     len_freqs = size(left_freqs, 1)
     estimated_freqs = calculate_freq_range(sampling_rate, N)
     #psd_bin_reduced = zeros(eltype(psd_bin), num_segments, len_freqs)
-    psd_bin_reduced = Array{eltype(psd_bin)}(undef, num_segments, len_freqs)
+    """psd_bin_reduced = Array{eltype(psd_bin)}(undef, num_segments, len_freqs)
     for i in 1:num_segments
         for j in 1:len_freqs
             count = 0
@@ -91,11 +95,23 @@ function summarize_psd(psd_bin::AbstractArray{<:Real, 2}, sampling_rate::Float64
             # divide by period, such non-sense
             psd_bin_reduced[i, j] /= (divide_by_period ? center_periods[j] : convert(eltype(psd_bin_reduced), count))
         end
+    end"""
+    psd_bin_reduced = zeros(eltype(psd_bin), len_freqs)
+    for i in 1:len_freqs
+        count = 0
+        for j in 1:N
+            if left_freqs[i] >= estimated_freqs[j] && estimated_freqs[j] >= right_freqs[i]
+                psd_bin_reduced[i] += psd_bin_temp[j]
+                count += 1
+            end
+        end
+
+        psd_bin_reduced[i] /= count
     end
 
     # Set unit to decibel (dB)
     psd_result = Array{eltype(psd_bin_reduced)}(undef, 4, len_freqs)
-    for i in 1:len_freqs
+    """for i in 1:len_freqs
         psd_result[1, i] = decibel(mean(psd_bin_reduced[:, i])) # mean
 
         min_and_max = extrema(psd_bin_reduced[:, i])
@@ -103,6 +119,12 @@ function summarize_psd(psd_bin::AbstractArray{<:Real, 2}, sampling_rate::Float64
         psd_result[3, i] = decibel(min_and_max[2]) # max
 
         psd_result[4, i] = decibel(median(psd_bin_reduced[:, i])) # median
+    end"""
+    for i in 1:len_freqs
+        psd_result[1, i] = decibel(psd_bin_reduced[i]) # mean
+        psd_result[2, i] = 0
+        psd_result[3, i] = 0
+        psd_result[4, i] = 0
     end
 
     return psd_result, center_periods
